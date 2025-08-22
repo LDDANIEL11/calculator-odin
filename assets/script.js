82126ef68595f60db450dfc4188426b7fe403872
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-23 01:07:29 +0330

    wrote down base codes for operations

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+function operate(str, a, b) {
+  if (str == "+") {
+    sum(a, b);
+  } else if (str == "-") {
+    subtract(a, b);
+  } else if (str == "*") {
+    multiply(a, b);
+  } else if ((str = "/")) {
+    divide(a, b);
+  }
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
