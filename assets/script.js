90c87c7d24c8fbd66be27e7f0b86088fbda88b45
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-25 02:05:12 +0330

    added an eventlistener for all digit numbers and DOT

--- assets/script.js
+++ assets/script.js
@@ -27,3 +27,28 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let numStored = "";
+let opStored = "";
+
+const numbers = document.querySelectorAll(".num");
+const container = document.querySelector(".container");
+
+numbers.forEach((num) => {
+  num.addEventListener("click", () => {
+    const value = num.textContent;
+    addNumbers(value);
+  });
+});
+
+function addNumbers(number) {
+  if (container.textContent == "0") container.textContent = number;
+  else if (container.textContent !== "0") container.textContent += number;
+}
+
+const operators = document.querySelectorAll(".operator");
+firstNumber = "";
+currentOperator = "";
+operators.forEach((op) => {
+  op.addEventListener("click", () => {});
+});
